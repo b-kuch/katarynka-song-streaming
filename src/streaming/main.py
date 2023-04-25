f1f9d64d1dab8d0dca210aff1f6a45d8f7a786d7
b-kuch <58431879+b-kuch@users.noreply.github.com>
2023-04-25 19:04:17 +0200

    remove cors handling

--- src/streaming/main.py
+++ src/streaming/main.py
@@ -1,19 +1,8 @@
-import time
 from fastapi import FastAPI
 from starlette.responses import StreamingResponse
-from fastapi.middleware.cors import CORSMiddleware
 from src.streaming.music import get_song, file_size
 
 app = FastAPI()
-
-# TODO: find out how cors should be handled
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 @app.get('/')
@@ -29,4 +18,3 @@ async def get_audio(song_id: str):
 
     size = file_size(song_id)
     return StreamingResponse(iter_file(), media_type="audio/mp3", headers={'Content-Length': str(size)})
-    # return Response(content=get_song2(song_id), media_type="audio/mp3")
